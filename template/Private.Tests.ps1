e4857e29ae32e1b7a95dad43bcaba3b071b069e4
Tony Pagliaro <Tony@Pagliaro.co>
2021-08-20 11:37:56 -0400

    Add private placeholder function. Also add related tests for private

--- template/Private.Tests.ps1
+++ template/Private.Tests.ps1
@@ -1,16 +1,45 @@
 Describe 'Private Tests' {
     
     # Import Private functions
-    Get-ChildItem $PSScriptRoot\..\Private\*.ps1 |
-        ForEach-Object {. $_.FullName}
+    BeforeAll {
+        $privItems = Get-ChildItem $PSScriptRoot\..\Private\*.ps1
+        $privItems | ForEach-Object {. $_.FullName}
+        $script:funcNames = @($privItems.BaseName)
+    }
     
     Context 'Test Private Functions' {
 
-        It 'Valid Value' {
-            $Valid = 'Valid'
-            $Valid | Should -Be $Valid
+        # Check for Private Functions in Test Memory Space
+        BeforeAll {
+            if ($privItems) {
+                foreach ($item in $privItems) {
+                    (Get-Command $item.BaseName).Name | Should -Be $item.BaseName
+                }
+            } else {
+                1 | Should -Be 1
+            }
         }
 
+        # Remove the tested item from the initial array
+        AfterEach {
+            $script:funcNames = $script:funcNames | Where-Object {$_ -ne $thisName}
+        }
+
+        # Unit tests ...
+
+        It 'Checks the placeholder file' {
+            $thisName = 'placeholder'
+            placeholder | Should -Be $thisName
+        }
+
+    }
+
+    Context 'Clean up' {
+
+        It 'Ensures all  private functions have tests' {
+            $script:funcNames | Should -BeNullOrEmpty
+            $script:funcNames | Should -Be $thisName
+        }
     }
 
 }
