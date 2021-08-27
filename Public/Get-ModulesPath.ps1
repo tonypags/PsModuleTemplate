c612c33b27ce873d8ba2fdf435588d3b89bd09df
Tony Pagliaro <Tony@Pagliaro.co>
2021-08-27 11:43:44 -0400

    Update function to support windows powershell desktop edition.

--- Public/Get-ModulesPath.ps1
+++ Public/Get-ModulesPath.ps1
@@ -2,12 +2,18 @@ function Get-ModulesPath {
     [CmdletBinding()]
     param ()
     
-    if ($IsMacOs -or $IsLinux) {
+    if ( $PSEdition -eq 'Core' -and ($IsMacOs -or $IsLinux) ) {
+        
         ($env:PSModulePath -split ':')[0]
-    } elseif ($IsWindows) {
+    
+    } elseif ($PSEdition -eq 'Desktop' -or $IsWindows) {
+    
         Join-Path (Split-Path $profile -Parent) "Modules"
+    
     } else {
+    
         Throw "Unsupported OS"
+    
     }
 
 }#END: function Get-ModulesPath {}
