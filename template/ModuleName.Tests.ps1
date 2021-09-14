8d9b565b46ad906ce7d54b3b5cf4a8456c85ceea
Tony Pagliaro <tony@pagliaro.co>
2021-09-14 11:24:12 -0400

    Update test templates, ensure all functions tested

--- template/ModuleName.Tests.ps1
+++ template/ModuleName.Tests.ps1
@@ -1,23 +1,43 @@
 Describe '<%=$PLASTER_PARAM_Name%> Tests' {
 
-    Import-Module "<%=$PLASTER_PARAM_Name%>" -ea 0 -Force
+    BeforeAll {
+        Import-Module "<%=$PLASTER_PARAM_Name%>" -ea 0 -Force
+        $script:thisModule = Get-Module -Name "<%=$PLASTER_PARAM_Name%>"
+        $script:funcNames = $thisModule.ExportedCommands.Values |
+            Where-Object {$_.CommandType -eq 'Function'} |
+            Select-Object -ExpandProperty Name
+    }
 
     Context 'Test Module import' {
 
-        It 'Module is imported' {
-            $Valid = Get-Module -Name '<%=$PLASTER_PARAM_Name%>'
-            $Valid.Name | Should -Be '<%=$PLASTER_PARAM_Name%>'
+        It 'Ensures module is imported' {
+            $script:thisModule.Name | Should -Be '<%=$PLASTER_PARAM_Name%>'
         }
 
     }
 
     Context 'Test <%=$PLASTER_PARAM_Name%> Functions' {
 
-        It 'Valid Value (sample test)' {
-            $Valid = 'Valid'
-            $Valid | Should -Be $Valid
+        # Remove the tested item from the initial array
+        AfterEach {
+            $script:funcNames = $script:funcNames | Where-Object {$_ -ne $script:thisName}
         }
 
+        It 'Valid Value (sample test)' {
+            $Valid = 'Get-Valid'
+            $Valid | Should -Be $Valid
+
+            $script:thisName = 'Get-Valid'
+        }
+
+    }
+
+    Context 'Clean up' {
+
+        It 'Ensures all public functions have tests' {
+            $script:funcNames | Should -BeNullOrEmpty
+        }
+        
     }
 
 }
