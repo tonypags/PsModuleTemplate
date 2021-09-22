08c17fe0f408d62f94af9c43c4bb2e07f6bda0c6
Tony Pagliaro <Tony@Pagliaro.co>
2021-09-22 16:52:26 -0400

    update PS1 template: remove variables prior to export

--- PsModuleTemplate.psm1
+++ PsModuleTemplate.psm1
@@ -2,6 +2,7 @@ $Public = Get-ChildItem $PSScriptRoot\Public\*.ps1
 Foreach ($File in $Public) {
     . $File.FullName
 }
+Remove-Variable -Name 'Public','File' -Force -ea 0
 Export-ModuleMember -Function * -Alias * -Variable *
 
 $Private = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ea 0
