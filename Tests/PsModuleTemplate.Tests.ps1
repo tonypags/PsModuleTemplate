08288c0ba99e7d13d2205b84840a7b87be59c524
Tony Pagliaro <tony@pagliaro.co>
2021-12-15 11:04:26 -0500

    update THIS MODULE'S test script to new template

--- Tests/PsModuleTemplate.Tests.ps1
+++ Tests/PsModuleTemplate.Tests.ps1
@@ -1,23 +1,43 @@
 ﻿Describe 'PsModuleTemplate Tests' {
 
-    Import-Module "PsModuleTemplate" -ea 0
+    BeforeAll {
+        Import-Module "PsModuleTemplate" -ea 0 -Force
+        $script:thisModule = Get-Module -Name "PsModuleTemplate"
+        $script:funcNames = $thisModule.ExportedCommands.Values |
+            Where-Object {$_.CommandType -eq 'Function'} |
+            Select-Object -ExpandProperty Name
+    }
 
     Context 'Test Module import' {
 
-        It 'Module is imported' {
-            $Valid = Get-Module -Name 'PsModuleTemplate'
-            $Valid.Name | Should -Be 'PsModuleTemplate'
+        It 'Ensures module is imported' {
+            $script:thisModule.Name | Should -Be 'PsModuleTemplate'
         }
 
     }
 
     Context 'Test PsModuleTemplate Functions' {
 
+        # Remove the tested item from the initial array
+        AfterEach {
+            $script:funcNames = $script:funcNames | Where-Object {$_ -ne $script:thisName}
+        }
+
         It 'Gets the modules root path' {
             $mod = Get-ModulesPath
             Test-Path $mod | Should -Be $true
+
+            $script:thisName = 'Get-ModulesPath'
         }
 
+    }
+
+    Context 'Clean up' {
+
+        It 'Ensures all public functions have tests' {
+            $script:funcNames | Should -BeNullOrEmpty
+        }
+        
     }
 
 }
