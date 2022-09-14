cfecb18559c4fbafe772e7a0be73c2de4a052cd2
Tony Pagliaro <tony@pagliaro.co>
2022-09-14 11:56:27 -0400

    new script

---
+++ Scripts/Convert-HelpSynopsisToHtml.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+Given a script/function path, the SYNOPSIS is converted to HTML.
+For special formatting rules, please visit https://pandoc.org/MANUAL.html
+.EXAMPLE
+& ./Convert-HelpSynopsisToHtml.ps1 -Path "C:\Backup-Scripts\RubrikSLAComplianceSummary.ps1"
+#>
+param($Path)
+if (Get-Command 'pandoc') {} else {
+    throw 'Prerequisite not found! Please install pandoc first.'
+}
+if (Get-Command 'dos2unix') {} else {
+    throw 'Prerequisite not found! Please install dos2unix first.'
+}
+if (Test-Path $Path) {} else {
+    if (get-module 'psguitools' -l) {
+        $Path = Show-GuiFilePicker -Single -outString -Title "Select a PS1 File" -Extension 'ps1'
+    } else {
+        $Path = Read-Host "Enter the full path to a PS1 File"
+    }
+}
+if (Test-Path $Path) {} else {
+    throw "Path not found: '$Path'"
+}
+
+$fName   = 'SYNOPSIS.md'
+$fPath   = Join-Path $env:TEMP $fName
+$tParent = Join-Path $env:USERPROFILE 'Downloads'
+$tPath   = Join-Path $tParent 'SYNOPSIS.html'
+
+(Get-Help $Path -Full).SYNOPSIS | Out-File $fPath
+
+dos2unix "$fPath"
+pandoc "$fPath" -t 'html' -i -o $tPath
+
+Try{Invoke-Item $tPath -ea 'Stop'} Catch {Get-Item $tPath}
